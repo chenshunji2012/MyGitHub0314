b265928bacd7b2fa08596c30d37f12dd240f8fd6
chenshunji <chenshunji@chenshunji-PC.uCloud.com>
2012-03-14 14:19:15 +0800

    delete2

---
+++ MyWebApp/MyWebApp/Class5.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebApp
+{
+    public class Class5
+    {
+        string a = "class5.cs";
+    }
+}
