1f9da333110575479c24a8e21c0909693ae741e0
chenshunji <chenshunji@chenshunji-PC.uCloud.com>
2012-03-14 13:53:36 +0800

    added class3.cs

---
+++ MyWebApp/MyWebApp/Class3.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyWebApp
+{
+    public class Class3
+    {
+        string msg = "1234567";
+    }
+}
